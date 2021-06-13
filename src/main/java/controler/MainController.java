dd9995c0c359c7c0979005a4d4d0904c34a4c9fe
Andrew <faraponoff@mail.ru>
2021-06-13 16:55:13 +0430

    13.06.2021

--- src/main/java/controler/MainController.java
+++ src/main/java/controler/MainController.java
@@ -8,9 +8,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class MainController {
 
     @GetMapping("/")
-    public String greeting(Model model) {
+    public String index(Model model) {
         model.addAttribute("title", "Главная страница");
-        return "home";
+        return "index";
     }
 
 }
