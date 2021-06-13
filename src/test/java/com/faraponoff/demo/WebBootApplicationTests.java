e4ec7a8d61e4ca1c913c5db6bd445d7cac92d268
Andrew <faraponoff@mail.ru>
2021-06-13 13:31:23 +0430

    Initial commit

---
+++ src/test/java/com/faraponoff/demo/WebBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.faraponoff.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
