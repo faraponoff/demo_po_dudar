1cd6e37f401f322a3afcf2a651e07f85e50ab555
faraponoff <faraponoff@mail.ru>
2021-06-22 21:35:29 +0300

    22.06.2021

---
+++ src/main/java/com/boots/Application.java
@@ -0,0 +1,14 @@
+package com.boots;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+//@ComponentScan
+//@EnableAutoConfiguration
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
